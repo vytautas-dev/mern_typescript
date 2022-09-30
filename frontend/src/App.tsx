25d75a5712f32810cae359947ddd21ef3494cc46
vytautas-dev <wojciech.szczygielski91@gmail.com>
2022-09-30 09:24:48 +0200

    toastify css fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Dashboard, Login, Register } from './pages';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 
 function App() {
